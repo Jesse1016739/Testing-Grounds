9d45ad62f0016a2a7be56f657b22abd42b26c023
Jesse <1016739@student.sae.edu.au>
2019-10-28 16:19:50 +1100

    Enemy Spawning "Feature"
    Read as "humorous fuck up"

---
+++ Assets/Scripts/enemySpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemySpawner : MonoBehaviour
+{
+    public float spawnTime;
+    public GameObject enemy;
+
+    public bool spawnToggle = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //StartCoroutine(BreakInSpawning());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            spawnToggle = !spawnToggle; 
+        }
+
+        if (spawnToggle == true)
+        {
+            StartCoroutine(BreakInSpawning());
+        }
+        
+    }
+
+    //This is the coroutine started earlier
+    IEnumerator BreakInSpawning()
+    {
+        yield return new WaitForSeconds(spawnTime);
+        Instantiate(enemy);
+    }
+
+
+}
