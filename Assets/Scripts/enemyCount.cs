ddbce1767f6b51e7341c4d8918ec499b10463a01
Jesse <1016739@student.sae.edu.au>
2019-11-01 13:48:21 +1100

    Trying to get an enemy counter working

---
+++ Assets/Scripts/enemyCount.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyCount : MonoBehaviour
+{
+    public float count;
+    public int maxEnemy;
+
+    public int enemiesLeft;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject[] enemyLimit = GameObject.FindGameObjectsWithTag("Mutant");
+        enemiesLeft = enemyLimit.Length;
+        
+    }
+}
